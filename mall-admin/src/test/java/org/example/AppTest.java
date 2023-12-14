7281663c49280404b0159b0e72a6f92d6e491b7e
喻云虎 <50434721+15623572586@users.noreply.github.com>
2023-12-14 21:19:40 +0800

    创建mall-admin子工程

--- mall-admin/src/test/java/org/example/AppTest.java
+++ mall-admin/src/test/java/org/example/AppTest.java
@@ -2,8 +2,13 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+/**
+ * 这个注解可以解决：Test ignored. java.lang.IllegalStateException: Found multiple @SpringBootConfiguration annotated classes [Generic bean:
+ */
+@ContextConfiguration(classes = MallAdminApplication.class)
 class DemoApplicationTests {
 
     @Test
